f6e6cb611dc9e4acf8114b18c251868011c77647
Kyle Galloway <kyle.galloway.1993@gmail.com>
2017-11-01 21:11:16 -0500

    Format using prettier

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var generate = require('./lib/generate');
-var Twitter = require('twitter');
-var http = require('http');
+var express = require("express");
+var generate = require("./lib/generate");
+var Twitter = require("twitter");
+var http = require("http");
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -13,33 +13,37 @@ var tweet = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 function makeTweet() {
     var content = generate();
-    tweet.post('statuses/update', { status: content }, function (error, tweet, res) {
+    tweet.post("statuses/update", { status: content }, function(
+        error,
+        tweet,
+        res
+    ) {
         if (error) throw error;
-        console.log('Posted tweet: \"' + content + '\"');
+        console.log('Posted tweet: "' + content + '"');
     });
 }
 
 function keepAlive() {
     try {
-        http.get('http://foodtruckbot1.herokuapp.com/');
-        console.log('GET request sent; kept alive.');
+        http.get("http://foodtruckbot1.herokuapp.com/");
+        console.log("GET request sent; kept alive.");
     } catch (e) {
         console.log(e);
     }
 }
 
-app.get('/', function (req, res) {
-    res.send('Congratulations, you sent a GET request!');
-    console.log('Received a GET request and sent a response');
+app.get("/", function(req, res) {
+    res.send("Congratulations, you sent a GET request!");
+    console.log("Received a GET request and sent a response");
 });
 
-app.listen(port, function () {
-    console.log('App now listening on port', port);
+app.listen(port, function() {
+    console.log("App now listening on port", port);
 });
 
 makeTweet();
